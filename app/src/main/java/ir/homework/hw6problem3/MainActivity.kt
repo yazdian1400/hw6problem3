9f72d025e28d92c479e5c28767b2507867e2dc8f
Maedeh <yazdian.oul@gmail.com>
2022-02-16 18:20:09 +0330

    add 2 top images

--- app/src/main/java/ir/homework/hw6problem3/MainActivity.kt
+++ app/src/main/java/ir/homework/hw6problem3/MainActivity.kt
@@ -2,10 +2,17 @@ package ir.homework.hw6problem3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ir.homework.hw6problem3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
+
 }
